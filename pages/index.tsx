21ac42d1cfc1194879264e62e690b4d3ca403894
Mohammadreza Alizadeh <official.mohammadrezaalizadeh@gmail.com>
2022-03-14 23:47:30 +0330

    Change file name

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-import HomePage from "../components/home";
+import HomePage from "../components/home/Home";
 import MainLayout from "../layouts/MainLayout";
 
 const Home: NextPage = () => {
