9fd74a4a53120791aca10caf2fc178a3db48a5a9
Mohammadreza Alizadeh <official.mohammadrezaalizadeh@gmail.com>
2022-03-14 23:23:39 +0330

    FINISH-Header

--- components/header/HeaderLink.tsx
+++ components/header/HeaderLink.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import classes from "./headerLink.module.scss";
+import { useRouter } from "next/router";
 
 interface HeaderLinkProps {
   title: String;
@@ -9,9 +10,14 @@ interface HeaderLinkProps {
 }
 
 const HeaderLink: React.FC<HeaderLinkProps> = ({ href, icon, title }) => {
+  
+  const router = useRouter()
+
+  const activeLink = href === router.asPath ? classes.linkActive: classes.link
+  
   return (
     <Link href={`${href}`}>
-      <a className={classes.link}>
+      <a className={activeLink}>
         <span id={classes.icon}>{icon}</span> <span id={classes.title}>{`${title}`}</span>
       </a>
     </Link>
