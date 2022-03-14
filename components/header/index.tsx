37767b98eb578e9f369bad78af1a15e612967217
Mohammadreza Alizadeh <official.mohammadrezaalizadeh@gmail.com>
2022-03-14 22:33:10 +0330

    ADD style to header

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -10,13 +10,22 @@ import {
 } from "@heroicons/react/outline";
 import Linkeden from "../../public/assets/icons/Linkeden";
 import GitHub from "../../public/assets/icons/GitHub";
+import Link from "next/link";
 const Header = () => {
   return (
     <header className={classes.container}>
-      <Logo id={classes.logo} />
+      <Link href="/">
+        <a>
+          <Logo id={classes.logo} />
+        </a>
+      </Link>
       <div className={classes.navigator}>
         <HeaderLink title="Home" href="/" icon={<HomeIcon />} />
-        <HeaderLink title="About" href="/about" icon={<QuestionMarkCircleIcon />} />
+        <HeaderLink
+          title="About"
+          href="/about"
+          icon={<QuestionMarkCircleIcon />}
+        />
         <HeaderLink title="Skills" href="/skills" icon={<CogIcon />} />
         <HeaderLink title="Contant" href="/contact" icon={<MailIcon />} />
       </div>
