10b50b43cc85f3a471ac8b77d357fef395b70033
Mohammadreza Alizadeh <official.mohammadrezaalizadeh@gmail.com>
2022-03-19 15:07:18 +0330

    Add About me Comp

---
+++ components/home/AboutMe.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from "./aboutMe.module.scss";
+const AboutMe = () => {
+  return (
+    <section className={classes.container}>
+      <h1>About Me</h1>
+      <p>
+          I'm Mohammadreza Alizadeh as a Front-end developer how like learn and 
+      </p>
+    </section>
+  );
+};
+
+export default AboutMe;
--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import AboutMe from "./AboutMe";
 import classes from "./home.module.scss";
 import Landpage from "./Landpage";
 const HomePage = () => {
   return (
     <section className={classes.container}>
       <Landpage />
+      <AboutMe />
     </section>
   );
 };
